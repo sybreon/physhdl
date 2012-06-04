ac1cfaecad620f1227b46de9e595b1e3143a6ffa
Shawn Tan <shawn.tan@aeste.my>
2012-06-04 19:43:30 +0800

    Added GPL3 license text.

--- rtl/vlog/mux4.v
+++ rtl/vlog/mux4.v
@@ -10,3 +10,18 @@ module mux4 (out, sel, s0, s1, s2, s3);
        default: out <= s3;    
      endcase // case (sel)
 endmodule // mux4
+
+// Copyright (C) 2012, Shawn Tan <shawn.tan@sybreon.com>
+// 
+// This is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this. If not, see <http://www.gnu.org/licenses/>.
