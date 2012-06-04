ad7f638f5d909569973a58c7d3f12ce02134dab8
Shawn Tan <shawn.tan@aeste.my>
2012-06-04 19:24:54 +0800

    Added full VHDL/VLOG code.

--- rtl/vhdl/mux4.vhd
+++ rtl/vhdl/mux4.vhd
@@ -1,9 +1,20 @@
-process (sel, s0, s1, s2, s3)
-begin
-  case sel is
-    when "00" => out <= s0;
-    when "01" => out <= s1;
-    when "10" => out <= s2;
-    when others => out <= s3;
-  end case;
-end process;
+entity mux4 is  
+  port (
+    out : out std_logic;
+    sel : in  std_logic_vector(1 downto 0);
+    s0, s1, s2, s3  : in  std_logic;   
+    ); 
+end mux4;
+architecture rtl of mux4 is
+begin  -- rtl
+  process (sel, s0, s1, s2, s3)
+  begin
+    case sel is
+      when "00" => out <= s0;
+      when "01" => out <= s1;
+      when "10" => out <= s2;
+      when others => out <= s3;
+    end case;
+  end process;
+end rtl;
+
--- rtl/vlog/mux4.v
+++ rtl/vlog/mux4.v
@@ -1,8 +1,12 @@
-always @(sel or s0 or s1 or s2 or s3)
-  case (sel)
-    2'b00: out <= s0;
-    2'b01: out <= s1;
-    2'b10: out <= s2;
-    default: out <= s3;    
-  endcase // case (sel)
-   
+module mux4 (out, sel, s0, s1, s2, s3);
+   output      out;   
+   input [1:0] sel;
+   input       s0, s1, s2, s3;
+   always @(sel or s0 or s1 or s2 or s3)
+     case (sel)
+       2'b00: out <= s0;
+       2'b01: out <= s1;
+       2'b10: out <= s2;
+       default: out <= s3;    
+     endcase // case (sel)
+endmodule // mux4
